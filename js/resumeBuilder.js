45dc5ff8a5d900b216ca6d4c8207f28effec2851
shawnajean <shawnajean@users.noreply.github.com>
2016-02-04 21:02:11 -0600

    add renderBio function to view. remove scripts hiding areas from index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,92 +1,130 @@
 $(function() {
 	var data = {
-		name: "Shawna Jean",
-		role: "Web Developer",
-		contacts: {
-			"mobile" : "555.555.5555",
-			"email" : "hello@shawnajroberts.com",
-			"github" : "shawnajroberts",
-			"twitter" : "shawnajeanr",
-			"blog" : "http://shawnajroberts.com",
-			"location" : "Chicago, IL"
+		bio: {
+			name: "Shawna Jean",
+			role: "Web Developer",
+			biopic: "http://shawnajroberts.files.wordpress.com/2014/05/shawna-cropped.jpg",
+			contacts: {
+				mobile : "555.555.5555",
+				email : "hello@shawnajroberts.com",
+				github : "shawnajroberts",
+				twitter : "shawnajeanr",
+				blog : "http://shawnajroberts.com",
+			}
 		},
-		welcomeMessage: "Welcome!",
 		skills: [
 			"awesomeness", "programming", "teaching", "C", "C++", "Java", "HTML", "CSS", "Bootstrap"
 		],
-		biopic: "http://shawnajroberts.files.wordpress.com/2014/05/shawna-cropped.jpg",
 		schools: [
 			{
-				"name" : "Hampshire College",
-				"location" : "Amherst, MA",
-				"degree" : "BA",
-				"majors" : ["Interpersonal Communication", "Computer Science", "Theater Management"],
-				"dates" : "2012",
-				"url" : "https://hampshire.edu"
+				name : "Hampshire College",
+				location : "Amherst, MA",
+				degree : "BA",
+				majors : ["Interpersonal Communication", "Computer Science", "Theater Management"],
+				dates : "2012",
+				url : "https://hampshire.edu"
 			}
 		],
 		onlineCourses: [
 			{
-				"title" : "JavaScript Basics",
-				"school" : "Udacity",
-				"date" : "2015",
-				"url" : "https://udacity.com/course/ud804"
+				title : "JavaScript Basics",
+				school : "Udacity",
+				date : "2015",
+				url : "https://udacity.com/course/ud804"
 			},
 			{
-				"title" : "Intro to HTML and CSS",
-				"school" : "Udacity",
-				"date" : "2015",
-				"url" : "https://www.udacity.com/course/ud304"
+				title : "Intro to HTML and CSS",
+				school : "Udacity",
+				date : "2015",
+				url : "https://www.udacity.com/course/ud304"
 			},
 			{
-				"title" : "Fundamentals of Design",
-				"school" : "Code School",
-				"date" : "2015",
-				"url" : "https://www.codeschool.com/courses/fundamentals-of-design"
+				title : "Fundamentals of Design",
+				school : "Code School",
+				date : "2015",
+				url : "https://www.codeschool.com/courses/fundamentals-of-design"
 			}
 		],
 		jobs: [
 			{
-				"employer" : "Automattic",
-				"title" : "Happiness Engineer",
-				"location" : "",
-				"dates" : "2014 - current",
-				"description" : "Build relationships based on trust which result in happy, passionate, loyal customers and colleagues. Help people use Automattic’s products, including WordPress.com, Jetpack, and Gravatar. Troubleshoot, investigate, and create detailed bug reports including screenshots, steps to reproduce, and number of users affected. Create, update and edit support documentation. Build a community of support by sharing knowledge and insight amongst team members.",
-				"url" : "https://automattic.com"
+				employer : "Automattic",
+				title : "Happiness Engineer",
+				location : "",
+				dates : "2014 - current",
+				description : "Build relationships based on trust which result in happy, passionate, loyal customers and colleagues. Help people use Automattic’s products, including WordPress.com, Jetpack, and Gravatar. Troubleshoot, investigate, and create detailed bug reports including screenshots, steps to reproduce, and number of users affected. Create, update and edit support documentation. Build a community of support by sharing knowledge and insight amongst team members.",
+				url : "https://automattic.com"
 			},
 			{
-				"employer" : "Hampshire College",
-				"title" : "Network Technician",
-				"location" : "Amherst, MA",
-				"dates" : "2012 - 2014",
-				"description" : "Assist Network Engineer in design, construction, and maintenance of campus network and phone infrastructure. Oversee physical network implementation. Provide support and troubleshooting for VoIP, POTS, CATV, and network infrastructure. Provided technical support to faculty and staff members.",
-				"url" : "https://hampshire.edu"
+				employer : "Hampshire College",
+				title : "Network Technician",
+				location : "Amherst, MA",
+				dates : "2012 - 2014",
+				description : "Assist Network Engineer in design, construction, and maintenance of campus network and phone infrastructure. Oversee physical network implementation. Provide support and troubleshooting for VoIP, POTS, CATV, and network infrastructure. Provided technical support to faculty and staff members.",
+				url : "https://hampshire.edu"
 			}
 		],
 		projects: [
 			{
-				"title" : "Mockup to Website",
-				"dates" : "2015",
-				"description" : "Develop a responsive website from a PDF design mockup.",
-				"images" : ["images/197x148.gif"],
-				"url" : "https://github.com/shawnajroberts/udacity-front-end-dev/tree/master/Projects/Project%201"
+				title : "Mockup to Website",
+				dates : "2015",
+				description : "Develop a responsive website from a PDF design mockup.",
+				images : ["images/197x148.gif"],
+				url : "https://github.com/shawnajroberts/udacity-front-end-dev/tree/master/Projects/Project%201"
 			}
 		]
 	};
 
 	var controller = {
 		init: function() {
-			view.render();
+			view.init();
+		},
+		getBio: function() {
+			return data.bio;
 		}
 	};
 
 	var view = {
+		init: function() {
+			var bio;
+
+			this.biopic = $('#biopic');
+			this.name = $('#name');
+			this.role = $('#role');
+			this.contactList = $('#topContacts');
+
+			view.render();
+		},
 		render: function() {
+			view.renderBio();
+		},
+		renderBio: function() {
+			// add bio info to page
+			bio = controller.getBio();
+			console.log( bio.contacts );
+
+			this.biopic.attr( 'src', bio.biopic );
+			this.name.text( bio.name );
+			this.role.text( bio.role );
+
+			//mobile
+			var formattedMobile = bio.contacts.mobile.replace(/\./g, "");
+			this.contactList.append('<li class="flex-item"><span class="blue-text">mobile</span><span class="white-text"><a href="callto:' + formattedMobile + '">' + bio.contacts.mobile + '</a></span></li>');
+			//email
+			this.contactList.append('<li class="flex-item"><span class="blue-text">email</span><span class="white-text"><a href="mailto:' + bio.contacts.email + '">' + bio.contacts.email + '</a></span></li>');
+
+			//github
+			this.contactList.append('<li class="flex-item"><span class="blue-text">github</span><span class="white-text"><a href="https://github.com/' + bio.contacts.github + '">' + bio.contacts.github + '</a></span></li>');
+
+			//twitter
+			this.contactList.append('<li class="flex-item"><span class="blue-text">twitter</span><span class="white-text"><a href="https://twitter.com/' + bio.contacts.twitter + '">' + bio.contacts.twitter + '</a></span></li>');
+
+			//blog
+			this.contactList.append('<li class="flex-item"><span class="blue-text">blog</span><span class="white-text"><a href="' + bio.contacts.blog + '">' + bio.contacts.blog + '</a></span></li>');
 
 		}
 	};
 
-
+	controller.init();
 });
 
 
@@ -102,24 +140,12 @@ $(function() {
 
 
 
-var bio = {
+/*var bio = {
 	"display" : function() {
-		$("#header").prepend(HTMLbioPic.replace("%data%", bio.biopic));
-		$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-		$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-
-		var formattedMobile = bio.contacts.mobile.replace(/\./g, "");
-		formattedMobile = HTMLmobile.replace(/%formattedData%/g, formattedMobile);
-		formattedMobile = formattedMobile.replace(/%data%/g, bio.contacts.mobile);
-		//$("#topContacts").append(formattedMobile);
 		$("#topContacts").append(HTMLemail.replace(/%data%/g, bio.contacts.email));
 		$("#topContacts").append(HTMLgithub.replace(/%data%/g, bio.contacts.github));
 		$("#topContacts").append(HTMLtwitter.replace(/%data%/g, bio.contacts.twitter));
 		$("#topContacts").append(HTMLblog.replace(/%data%/g, bio.contacts.blog));
-		//$("#topContacts").append(HTMLlocation.replace(/%data%/g, bio.contacts.location));
-
-
-		//$("#header").append(HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage));
 
 		if( bio.skills.length > 0 ) {
 			$("#header").append(HTMLskillsStart);
@@ -138,7 +164,6 @@ var bio = {
 }
 
 var education = {
-	,
 	"display" : function() {
 		for( school in education.schools ) {
 			$("#education").append(HTMLschoolStart);
@@ -204,4 +229,4 @@ $("#mapDiv").append(googleMap);
 
 $(document).click(function(loc) {
 	logClicks(loc.pageX, loc.pageY);
-});
+});*/
